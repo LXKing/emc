a38c2a5dd9a02379060c922826901a4590f8a5bb
lichao <lichao@huak.com.cn>
2017-05-17 15:06:22 +0800

    js

--- emc-platform/src/main/webapp/static/Hplus/js/content.js
+++ emc-platform/src/main/webapp/static/Hplus/js/content.js
@@ -140,35 +140,3 @@ $(document).ready( function (e) {
 });
 
 var treeNodeClick = function(e,treeId,treeNode){ };
-
-
-
-function openLayer(url,title,form,width,height){
-    if(width==null||width=='undefined'){
-        width = top.document.body.clientWidth*0.7+"px";
-    }
-    if(height==null||height=='undefined'){
-        height = top.document.body.clientHeight*0.85+"px";
-    }
-
-    var $top = $(top.document);
-    var layerDiv = '<div id="layer-div"></div>';
-    $top.find('body').append(layerDiv);
-    $.get(url, function (result) {
-        $top.find("#layer-div").html(result);
-        top.layer.open({
-            area: [width, height],
-            type: 1,
-            title: title,
-            btn: ['保存', '取消'],
-            yes: function () {
-                $top.find("#"+form).submit();
-            },
-            skin: 'layer-ext-moon', //样式类名
-            closeBtn: 1, //不显示关闭按钮
-            shift: 2,//出场动画
-            shadeClose: true, //开启遮罩关闭
-            content: $top.find("#layer-div")
-        });
-    });
-}
