1831fe6d126886e312fd1c032820ae1f2b09a9ee
liurunming <67859086@qq.com>
2017-05-17 15:14:09 +0800

    Oncenet -- Dao

---
+++ emc-dao/src/main/java/com/huak/org/dao/OncenetDao.java
@@ -0,0 +1,23 @@
+package com.huak.org.dao;
+
+import com.huak.org.model.Feed;
+import com.huak.org.model.Oncenet;
+
+import java.util.List;
+import java.util.Map;
+
+public interface OncenetDao {
+    int deleteByPrimaryKey(String id);
+
+    int insert(Oncenet record);
+
+    int insertSelective(Oncenet record);
+
+    Oncenet selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(Oncenet record);
+
+    int updateByPrimaryKey(Oncenet record);
+
+    List<Oncenet> selectPageByMap(Map<String,Object> paramsMap);
+}
