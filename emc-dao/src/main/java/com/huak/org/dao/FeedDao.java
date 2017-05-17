fc84cb83e870049becd859495fa689077bd930ad
liurunming <67859086@qq.com>
2017-05-17 14:45:24 +0800

    热源Dao

--- emc-dao/src/main/java/com/huak/org/dao/FeedDao.java
+++ emc-dao/src/main/java/com/huak/org/dao/FeedDao.java
@@ -1,7 +1,11 @@
 package com.huak.org.dao;
 
+import com.huak.auth.model.Role;
 import com.huak.org.model.Feed;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface FeedDao {
@@ -16,4 +20,6 @@ public interface FeedDao {
     int updateByPrimaryKeySelective(Feed record);
 
     int updateByPrimaryKey(Feed record);
+
+    List<Feed> selectPageByMap(Map<String,Object> paramsMap);
 }
