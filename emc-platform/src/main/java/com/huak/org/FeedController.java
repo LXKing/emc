275ce64624b6b2e6a568f4fb39e564ea296554fd
liurunming <67859086@qq.com>
2017-05-17 14:47:29 +0800

    feed--platform

---
+++ emc-platform/src/main/java/com/huak/org/FeedController.java
@@ -0,0 +1,133 @@
+package com.huak.org;
+
+import com.alibaba.fastjson.JSONObject;
+import com.huak.auth.model.Role;
+import com.huak.base.dao.DateDao;
+import com.huak.common.Constants;
+import com.huak.common.UUIDGenerator;
+import com.huak.common.page.Page;
+import com.huak.org.model.Feed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+/**
+ * Copyright (C), 2009-2012, 北京华热科技发展有限公司.<BR>
+ * ProjectName:emc<BR>
+ * File name:  com.huak.org<BR>
+ * Author:  Administrator  <BR>
+ * Project:emc    <BR>
+ * Version: v 1.0      <BR>
+ * Date: 2017/5/16<BR>
+ * Description:     <BR>
+ * Function List:  <BR>
+ */
+
+@Controller
+@RequestMapping("/feed")
+public class FeedController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Resource
+    private FeedService feedService;
+
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String listPage() {
+        logger.info("转至系统热源列表页");
+        return "/org/feed/list";
+    }
+
+    @RequestMapping(value = "/listpage", method = RequestMethod.PATCH)
+    @ResponseBody
+    public String list(@RequestParam Map<String, Object> paramsMap, Page page) {
+        logger.info("热源列表页分页查询");
+
+        JSONObject jo = new JSONObject();
+        try {
+            jo.put(Constants.LIST, feedService.queryByPage(paramsMap, page));
+        } catch (Exception e) {
+            logger.error("热力站列表页分页查询异常" + e.getMessage());
+        }
+        return jo.toJSONString();
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String addPage() {
+        return "/org/feed/add";
+    }
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String edit(Model model, @PathVariable("id") String id) {
+        logger.info("跳转修改热源页");
+        try {
+            Feed feed = feedService.selectByPrimaryKey(id);
+            model.addAttribute("feed", feed);
+        } catch (Exception e) {
+            logger.error("跳转修改热源页异常" + e.getMessage());
+        }
+        return "/org/feed/edit";
+    }
+
+    @RequestMapping(value = "/addvalue", method = RequestMethod.POST)
+    @ResponseBody
+    public String add(Feed feed, HttpServletRequest request) {
+        logger.info("添加热源");
+
+        JSONObject jo = new JSONObject();
+        jo.put(Constants.FLAG, false);
+        try {
+            // TODO 添加session，创建者
+            HttpSession session = request.getSession();
+
+            feed.setId(UUIDGenerator.getUUID());
+            feedService.insertSelective(feed);
+            jo.put(Constants.FLAG, true);
+            jo.put(Constants.MSG, "添加热源成功");
+        } catch (Exception e) {
+            logger.error("添加热源异常" + e.getMessage());
+            jo.put(Constants.MSG, "添加热源失败");
+        }
+        return jo.toJSONString();
+    }
+    @RequestMapping(value = "/editvalue", method = RequestMethod.POST)
+    @ResponseBody
+    public String editValue(Feed feed) {
+        logger.info("修改热源");
+
+        JSONObject jo = new JSONObject();
+        jo.put(Constants.FLAG, false);
+        try {
+            int i = feedService.updateByPrimaryKeySelective(feed);
+            jo.put(Constants.FLAG, true);
+            jo.put(Constants.MSG, "修改热源成功");
+        } catch (Exception e) {
+            logger.error("修改热源异常" + e.getMessage());
+            jo.put(Constants.MSG, "修改热源失败");
+        }
+        return jo.toJSONString();
+    }
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public String deleteFeed(@PathVariable("id") String id) {
+        logger.info("删除热源");
+
+        JSONObject jo = new JSONObject();
+        jo.put(Constants.FLAG, false);
+        try {
+            feedService.deleteByPrimaryKey(id);
+            jo.put(Constants.FLAG, true);
+            jo.put(Constants.MSG, "删除热源成功");
+        } catch (Exception e) {
+            logger.error("删除热源异常" + e.getMessage());
+            jo.put(Constants.MSG, "删除热源失败");
+        }
+        return jo.toJSONString();
+    }
+}
--- emc-platform/src/main/webapp/script/org/huak.org.feed.list.js
+++ emc-platform/src/main/webapp/script/org/huak.org.feed.list.js
@@ -1,111 +1,355 @@
 /**
  * Copyright (C), 2009-2012, 北京华热科技发展有限公司.<BR>
- * Author:  bin  <BR>
+ * Author:  lichao  <BR>
  * Project:eccp    <BR>
  * Version: v 1.0      <BR>
- * Date: 2016/8/25<BR>
+ * Date: 2016/8/30<BR>
  */
+var bTable;
 $(function () {
-    init();
-    getFeedList();
-});
-function init() {
-    var config = {
-        '.chosen-select': {},
-        '.chosen-select-deselect': {
-            allow_single_deselect: true
+//    var setting = {
+//        view: {
+//            addHoverDom: addHoverDom,
+//            removeHoverDom: removeHoverDom,
+//            selectedMulti: false,
+//            fontCss:{color:"blue"}
+//        },
+//        check: {
+//            enable: true
+//        },
+//        data: {
+//            simpleData: {
+//                enable: true,
+//                idKey: "id",
+//                pIdKey: "pId",
+//                system:"Name",
+//                rootPId: ""
+//            }
+//        },
+//        async : { // 是否异步加载 相当于ajax
+//
+//            enable : true//设置 zTree 是否开启异步加载模式
+//            //默认值：false
+//
+//        },
+//
+//        edit: {
+//            enable: true
+//        },
+//        callback: {
+//            beforeRemove:beforeRemove,//点击删除时触发，用来提示用户是否确定删除
+//            beforeEditName: beforeEditName,//点击编辑时触发，用来判断该节点是否能编辑
+//            beforeRename:beforeRename,//编辑结束时触发，用来验证输入的数据是否符合要求
+//            onRemove:onRemove,//删除节点后触发，用户后台操作
+//            onRename:onRename,//编辑后触发，用于操作后台
+//            beforeDrag:beforeDrag, //用户禁止拖动节点
+//            onClick:clickNode//点击节点触发的事件
+//
+//        }
+//    };
+//    function ztreeValue(){
+//        $.ajax({
+//            type: "get",
+//            url: _platform+"/org/ztreeValue",
+//            data: {},
+//            dataType: "json",
+//            success: function (data) {
+//                //console.log(data);
+//                var nodes='';
+//                var zNodes ='[';
+//                for (var i=0;i<data.length;i++){
+//                    var orgName='"' + data[i].orgName + '"';
+//                    var id='"' + data[i].id + '"';
+//                    var pid='"' + data[i].pOrgId + '"';
+//                    zNodes+="{ id:"+id+", pId:"+pid+", name:"+orgName+", open:true},";
+//                };
+//                var newnodes=zNodes.substring(0,zNodes.length-1);
+//                nodes= newnodes+"]"
+//                //alert(nodes);
+//                $.fn.zTree.init($("#treeDemo"), setting, eval("(" + nodes + ")"));
+//            },
+//            error: function(data) {
+//                alert("data is erro");
+//            }
+//        });
+//    }
+
+    bTable = $('#feed-table-list').bootstrapTable({
+        height: getHeight() + 30,//高度
+        cache: false,//禁用 AJAX 数据缓存
+        url: _platform + '/feed/listpage',//获取数据的Servlet地址
+        method: 'post',//使用POST请求到服务器获取数据
+        contentType: "application/x-www-form-urlencoded",
+        dataType: "json",
+        idField: "ID",
+        pagination: true,//是否分页
+        pageSize: 10,//每页显示的记录数
+        pageNumber: 1,//当前第几页
+        pageList: [10, 30, 50],//记录数可选列表
+        search: false,  //是否启用查询
+        striped: true,//表格显示条纹
+        //showColumns: false,//不显示隐藏列
+        sidePagination: "server", //服务端请求
+        //设置为undefined可以获取pageNumber，pageSize，searchText，sortName，sortOrder
+        //设置为limit可以获取limit, offset, search, sort, order
+        queryParamsType: "undefined",
+        queryParams: function queryParams(params) {
+            var param = {
+                roleName:$('input[name="feedType"]').val(),
+                _method: "PATCH",
+                pageNumber: params.pageNumber,
+                pageSize: params.pageSize
+            };
+            return param;
+        }, formatLoadingMessage: function () {
+            return "请稍等，正在加载中...";
         },
-        '.chosen-select-no-single': {
-            disable_search_threshold: 10
+        responseHandler: function (res) {
+            return {
+                "rows": res.list.list,
+                "total": res.list.page.total
+            };
         },
-        '.chosen-select-no-results': {
-            no_results_text: 'Oops, nothing found!'
-        },
-        '.chosen-select-width': {
-            width: "10%"
+        columns: [
+            {
+                field: 'id', title: 'ID', visible: false
+            },
+            /*{
+             checkbox: true
+             },*/
+            {
+                title: '序号',
+                field: 'sn',
+                align: 'center',
+                formatter:function(value,row,index){
+                    return index+1;
+                }
+            },
+            {
+                title: '热源性质',
+                field: 'feedType',
+                align: 'center'
+            },
+            {
+                title: '供热类型',
+                field: 'feedType',
+                align: 'center'
+            },
+            {
+                title: '装机容量',
+                field: 'installCapacity',
+                align: 'center'
+            },
+            {
+                title: '供热能力',
+                field: 'heatCapacity',
+                align: 'center'
+            },
+            {
+                title: '汽机数量',
+                field: 'steamturbineNum',
+                align: 'center'
+            },
+            {
+                title: '省',
+                field: 'provinceId',
+                align: 'center'
+            },
+            {
+                title: '市',
+                field: 'cityId',
+                align: 'center'
+            },
+            {
+                title: '县',
+                field: 'countyId',
+                align: 'center'
+            },
+            {
+                title: '乡',
+                field: 'townId',
+                align: 'center'
+            },
+            {
+                title: '村',
+                field: 'villageId',
+                align: 'center'
+            },
+            {
+                title: '详细地址',
+                field: 'addr',
+                align: 'center'
+            },
+            {
+                title: '经度',
+                field: 'lng',
+                align: 'center'
+            },
+            {
+                title: '维度',
+                field: 'lat',
+                align: 'center'
+            },
+            {
+                title: '公建面积',
+                field: 'publicArea',
+                align: 'center'
+            },
+            {
+                title: '居民面积',
+                field: 'dwellArea',
+                align: 'center'
+            },
+            {
+                title: '锅炉数量',
+                field: 'boilerNum',
+                align: 'center',
+                formatter:function(value,row,index){
+                    if(value.length>20){
+                        return '<span title="'+value+'">'+value.substr(0,20)+'</span>';
+                    }
+                    return value;
+                }
+            },
+            {
+                title: '操作',
+                field: 'opt',
+                align: 'center' ,
+                formatter:function(value,row,index){
+                    return '<a title="编辑" class="btn btn-xs btn-info top-layer-min" layer-form-id="feedEditForm" layer-title="编辑热源" layer-url="'+_platform+'/feed/edit/'+row.id+'"> <i class="fa fa-edit"></i></a>&nbsp;' +
+                        '<a title="删除" class="btn btn-xs btn-danger" onclick="deleteFeed(&quot;'+row.id+'&quot;)"><i class="fa fa-trash-o"></i></a>&nbsp;';
+                }
+            }
+
+        ]
+
+
+    });
+
+    //页面说明
+    console.info("页面说明：\n1.系统默认2个角色为超级管理员和企业管理员；\n" +
+        "2.管理员用户才能进行管理且只能看到自己创建的角色；\n" +
+        "功能：\n" +
+        "【添加】【删除】【修改】【授权权限】【检索】【重置】【导出EXCEL】\n" +
+        "字段：\n角色主键、角色名称、角色备注\n" +
+        "创建人、创建人组织、创建时间、修改人、修改人组织、修改时间、是否删除" );
+
+    //日期范围限制
+    var start = {
+        elem: '#start',
+        format: 'YYYY/MM/DD hh:mm:ss',
+        //min: laydate.now(), //设定最小日期为当前日期
+        max: '2099-06-16 23:59:59', //最大日期
+        istime: true,
+        istoday: false,
+        choose: function (datas) {
+            end.min = datas; //开始日选好后，重置结束日的最小日期
+            end.start = datas //将结束日的初始值设定为开始日
         }
-    }
-    for (var selector in config) {
-        $(selector).chosen(config[selector]);
-    }
+    };
+    var end = {
+        elem: '#end',
+        format: 'YYYY/MM/DD hh:mm:ss',
+        max: '2099-06-16 23:59:59',
+        istime: true,
+        istoday: false,
+        choose: function (datas) {
+            start.max = datas; //结束日选好后，重置开始日的最大日期
+        }
+    };
+//    laydate(start);
+//    laydate(end);
+
+    //下拉框js
+    $(".chosen-select").chosen();
+
+});
+function search(){
+    $('#feed-table-list').bootstrapTable('refresh');
 }
 
-    /* 查询字典列表 */
-    function getFeedList() {
-        var index = layer.load(1, {
+//layer
+function deleteFeed(id) {
+    top.layer.confirm('您是否确定删除热源？', {
+        btn: ['确定', '取消'] //按钮
+    }, function () {
+        var index = top.layer.load(1, {
             shade: [0.1, '#fff'] //0.1透明度的白色背景
         });
         $.ajax({
-            url: ctx + '/feed/list',
-            timeout: 30000,
-            type: 'POST',//注意在传参数时，加：_method:'PATCH'　将对应后台的PATCH请求方法
+            url: _platform + '/feed/delete/' + id,
+            type: 'DELETE',
             dataType: 'json',
-            data: $("#feed-form").serialize(),
             success: function (result) {
-                $(".pagination").pagination({
-                    pageNo: result.list.page.curPage,
-                    rowTotal: result.list.page.totalRow,
-                    _callBack: getFeedList
-                });
-                // 附上模板
-                $("#feed-tbody").setTemplateElement("tpl-list");
-                // 给模板加载数据
-                $("#feed-tbody").processTemplate(result.list);
-                $('.i-checks').iCheck({
-                    checkboxClass: 'icheckbox_square-green',
-                    radioClass: 'iradio_square-green'
-                });
-                layer.close(index);
-            },
-            error: function () {
-                layer.close(index);
-                layer.msg("加载失败");
-            },
-            complete: function (XMLHttpRequest, status) { //请求完成后最终执行参数
-                layer.close(index);
-                if (status == 'timeout') {//超时,status还有success,error等值的情况
-                    layer.msg("加载超时");
+                if (result.flag) {
+                    top.layer.closeAll();
+                    top.layer.msg(result.msg);
+                    $('#feed-table-list').bootstrapTable("refresh");
+                } else {
+                    top.layer.close(index);
+                    top.layer.msg(result.msg);
                 }
             }
         });
-    }
+    });
+}
 
+function addFeed() {
+    $.get(_platform + '/feed/add', function (result) {
+        $('#feed-layer-div').html(result);
+    });
+    layer.open({
+        area: ['800px', '580px'],
+        type: 1,
+        title: '添加热源',
+        btn: ['保存', '取消'],
+        yes: function () {
+            $("#roleAddForm").submit();
+        },
+        skin: 'layer-ext-moon', //样式类名
+        closeBtn: 1, //不显示关闭按钮
+        shift: 2,
+        shadeClose: true, //开启遮罩关闭
+        content: $('#feed-layer-div')
+    });
+}
 
-    /*查询*/
-    var findAll = function () {
-        $("input[id ='pageNo']").val(1);
-        getFeedList();
-    }
+function editRole(id) {
+    $.get(_platform + '/role/edit/' + id, function (result) {
+        $('#role-layer-div').html(result);
+    });
+    layer.open({
+        area: ['800px', '580px'],
+        type: 1,
+        title: '编辑角色',
+        btn: ['保存', '取消'],
+        yes: function () {
+            $("#roleEditForm").submit();
+        },
+        skin: 'layer-ext-moon', //样式类名
+        closeBtn: 1, //不显示关闭按钮
+        shift: 2,
+        shadeClose: true, //开启遮罩关闭
+        content: $('#role-layer-div')
+    });
+}
 
-
-var delFeed = function (id){
-    var ids;
-    if(id == undefined){
-        ids= getCheckValues("feedListTable");
-        if (ids.length == 0) {
-            layer.msg("至少选择一条记录进行删除！");
-            return false;
-        }
-    }else{
-        ids = id;
-    }
-    layer.confirm('是否删除数据？', {
-        btn: ['删除', '取消'] //按钮
+function deleteRole(id) {
+    layer.confirm('您是否确定删除角色？', {
+        btn: ['确定', '取消'] //按钮
     }, function () {
         var index = layer.load(1, {
             shade: [0.1, '#fff'] //0.1透明度的白色背景
         });
         $.ajax({
-            url: ctx + '/feed/delete',
-            type: 'POST',
+            url: _platform + '/role/delete/' + id,
+            type: 'DELETE',
             dataType: 'json',
-            data: {feedId: ids},
             success: function (result) {
                 if (result.flag) {
                     layer.closeAll();
                     layer.msg(result.msg);
-                    getFeedList();
-
+                    $('#role-table-list').bootstrapTable("refresh");
                 } else {
                     layer.close(index);
                     layer.msg(result.msg);
@@ -115,9 +359,21 @@ var delFeed = function (id){
     });
 }
 
+function roleAuthPage() {
+    var id = getCheckValues();
+    if (id.length == 0) {
+        layer.msg("请选择要授权的角色");
+        return false;
+    }
+    if (id.split(',').length > 1) {
+        layer.msg("请选择一个角色进行授权");
+        return false;
+    }
+    $("#content-main").empty().load(_platform + '/role/auth?id=' + id);
+}
 
 
-
-
-
-
+function treeNodeClick(e,treeId,treeNode){
+    alert(123);
+    search();
+}
