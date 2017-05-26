04dffafcaf601dbcba59894762cafbeeace75711
lichao <lichao@huak.com.cn>
2017-05-26 09:57:15 +0800

    创建控制

---
+++ emc-web/src/main/java/com/huak/web/home/EnergyMonitorController.java
@@ -0,0 +1,20 @@
+package com.huak.web.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Copyright (C), 2009-2012, 北京华热科技发展有限公司.<BR>
+ * ProjectName:emc<BR>
+ * File name:  com.huak.web.home<BR>
+ * Author:  lichao  <BR>
+ * Project:emc    <BR>
+ * Version: v 1.0      <BR>
+ * Date: 2017/5/26<BR>
+ * Description:  首页-二级页面- 集团总能耗  <BR>
+ * Function List:  <BR>
+ */
+@Controller
+@RequestMapping("/energy/monitor")
+public class EnergyMonitorController {
+}
