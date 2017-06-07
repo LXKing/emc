d3f5bb8399badc63ace1826a48eb6d08e138cffc
sunbinbin <2267506756@qq.com>
2017-06-07 10:18:12 +0800

    热源

--- emc-dao/src/main/java/com/huak/auth/dao/EmployeeDao.java
+++ emc-dao/src/main/java/com/huak/auth/dao/EmployeeDao.java
@@ -14,17 +14,66 @@ public interface EmployeeDao {
      */
     List<Employee> selectPageByMap(Map<String, Object> paramsMap);
 
+    /**
+     * 根据id删除数据
+     * @param id
+     * @return
+     */
     int deleteByPrimaryKey(Long id);
 
+    /**
+     * 新增
+     * @param record
+     * @return
+     */
     int insert(Employee record);
 
+    /**
+     * 新增
+     * @param record
+     * @return
+     */
     int insertSelective(Employee record);
 
+    /**
+     * 根据id选择实体
+     * @param id
+     * @return
+     */
     Employee selectByPrimaryKey(Long id);
 
+    /**
+     * 修改
+     * @param record
+     * @return
+     */
     int updateByPrimaryKeySelective(Employee record);
 
+    /**
+     * 修改
+     * @param record
+     * @return
+     */
     int updateByPrimaryKey(Employee record);
 
+    /**
+     * 根据id数组批量删除
+     * @param ids
+     * @return
+     */
     int delete(String[] ids);
+
+    /**
+     * 删除数据，修改状态，软删除
+     * @param ids
+     * @return
+     */
+    int deleteByStatus(String ids);
+
+    /**
+     * 导出
+     * @param paramsMap
+     * @return
+     */
+    List<Map<String,Object>> export(Map<String, Object> paramsMap);
 }
