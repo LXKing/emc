0382a18eee1ea22f0ca2e3dbf4c748fd9dab913d
liurunming <67859086@qq.com>
2017-07-14 09:12:51 +0800

    接口提交

---
+++ emc-task-api/src/main/java/com/huak/Temp/TempService.java
@@ -0,0 +1,31 @@
+package com.huak.temp;
+
+import com.huak.task.model.Temperature;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Copyright (C), 2009-2012, 北京华热科技发展有限公司.<BR>
+ * ProjectName:emc<BR>
+ * File name:  com.huak.task<BR>
+ * Author:  Administrator  <BR>
+ * Project:emc    <BR>
+ * Version: v 1.0      <BR>
+ * Date: 2017/7/13<BR>
+ * Description:     <BR>
+ * Function List:  <BR>
+ */
+
+public interface TempService {
+
+    /**
+     * 确认室温内是否存在将要导入的数据
+     */
+    public List<Temperature>  isExsistTemp(Map<String,Object> map);
+
+    /**
+     * 确认后导入室温数据
+     */
+    public Map<String, Object>  insertTemp(Temperature temp);
+}
