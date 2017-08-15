38d5c0443eed181e62ee772209dad08c429b8d29
lichao <lichao@huak.com.cn>
2017-08-15 16:51:43 +0800

    加逗号

--- emc-web/src/main/webapp/static/js/huak.cookie.js
+++ emc-web/src/main/webapp/static/js/huak.cookie.js
@@ -22,12 +22,12 @@ function getCookie(c_name)
 {
     if (document.cookie.length>0)
     {
-        c_start=document.cookie.indexOf(c_name + "=")
+        c_start=document.cookie.indexOf(c_name + "=");
         if (c_start!=-1)
         {
-            c_start=c_start + c_name.length+1
-            c_end=document.cookie.indexOf(";",c_start)
-            if (c_end==-1) c_end=document.cookie.length
+            c_start=c_start + c_name.length+1;
+            c_end=document.cookie.indexOf(";",c_start);
+            if (c_end==-1) c_end=document.cookie.length;
             return decodeURI(document.cookie.substring(c_start,c_end),"utf-8");
         }
     }
