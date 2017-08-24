69a5e8b67fc04357f6c1ce264723b86639b84787
lichao <lichao@huak.com.cn>
2017-08-24 10:12:05 +0800

    工具提交

--- emc-common/src/main/java/com/huak/common/CollectionUtil.java
+++ emc-common/src/main/java/com/huak/common/CollectionUtil.java
@@ -61,4 +61,23 @@ public class CollectionUtil {
         return map;
     }
 
+    /**
+     * 封装数据方法
+     * 由于emc系统有大量的本期和同期的折线图，故有此公共方法
+     * 限制：横坐标每天时间、本期折线、同期折线
+     * @param start 开始时间
+     * @param end 结束时间
+     * @param thisList 本期数据List<Map<String,Object>> map中一定存在key{dayDate,dayValue}
+     * @param sameList 同期数据List<Map<String,Object>> map中一定存在key{dayDate,dayValue}
+     * @return Map<String,Object>
+     *     key = xdatas value = List<String>{}   横坐标
+     *     key = aline value = List<String>{}    第一条线，本期
+     *     key = bline value = List<String>{}    第二条线，同期
+     */
+    public static Map<String,Object> packageDataLine(String start,String end,List<Map<String,Object>> thisList,List<Map<String,Object>> sameList){
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        return map;
+    }
+
 }
