7a68acf2d9d9058610ae934ba4dca41808cc7453
lichao <lichao@huak.com.cn>
2017-08-24 16:34:33 +0800

    为0判断

--- emc-common/src/main/java/com/huak/common/utils/DoubleUtils.java
+++ emc-common/src/main/java/com/huak/common/utils/DoubleUtils.java
@@ -88,6 +88,9 @@ public class DoubleUtils {
         }
         BigDecimal b1 = new BigDecimal(Double.toString(v1));
         BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        if(b1.equals(0)||b2.equals(0)){
+            return 0;
+        }
         return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 
