da3189e8bda198b5a1cc43d3219ff4c965631178
liurunming <67859086@qq.com>
2017-08-29 15:37:52 +0800

    project

--- emc-dao/src/main/java/com/huak/mdc/model/MeterCollect.java
+++ emc-dao/src/main/java/com/huak/mdc/model/MeterCollect.java
@@ -3,6 +3,8 @@ package com.huak.mdc.model;
 import java.io.Serializable;
 
 public class MeterCollect implements Serializable{
+
+    private static final long serialVersionUID = -2047226685880359589L;
     private String id;
 
     private String code;
@@ -18,6 +20,8 @@ public class MeterCollect implements Serializable{
     private Byte istotal;
 
     private String unitId;
+
+    private String unitname;
 
     private Byte unitType;
 
@@ -37,7 +41,7 @@ public class MeterCollect implements Serializable{
 
     private String comId;
 
-    public MeterCollect(String id, String code, String name, String serialNo, String energyTypeId, Byte isreal, Byte istotal, String unitId, Byte unitType, Byte isauto, String depict, String tag, Byte coef, String formula, Byte isprestore, Byte isdelete, String comId) {
+    public MeterCollect(String id, String code, String name, String serialNo, String energyTypeId, Byte isreal, Byte istotal, String unitId, String unitname, Byte unitType, Byte isauto, String depict, String tag, Byte coef, String formula, Byte isprestore, Byte isdelete, String comId) {
         this.id = id;
         this.code = code;
         this.name = name;
@@ -46,6 +50,7 @@ public class MeterCollect implements Serializable{
         this.isreal = isreal;
         this.istotal = istotal;
         this.unitId = unitId;
+        this.unitname=unitname;
         this.unitType = unitType;
         this.isauto = isauto;
         this.depict = depict;
@@ -123,6 +128,14 @@ public class MeterCollect implements Serializable{
 
     public void setUnitId(String unitId) {
         this.unitId = unitId == null ? null : unitId.trim();
+    }
+
+    public String getUnitname() {
+        return unitname;
+    }
+
+    public void setUnitname(String unitname) {
+        this.unitname = unitname;
     }
 
     public Byte getUnitType() {
