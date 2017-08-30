d030f96423d38dc08ceb42fdf6ad7808f8740647
liurunming <67859086@qq.com>
2017-08-30 10:00:14 +0800

    project

--- emc-dao/src/main/java/com/huak/mdc/dao/MeterCollectDao.java
+++ emc-dao/src/main/java/com/huak/mdc/dao/MeterCollectDao.java
@@ -1,11 +1,13 @@
 package com.huak.mdc.dao;
 
 import com.huak.mdc.model.MeterCollect;
+import org.springframework.stereotype.Repository;
 
 
 import java.util.List;
 import java.util.Map;
 
+@Repository
 public interface MeterCollectDao {
     int deleteByPrimaryKey(String id);
 
@@ -22,4 +24,10 @@ public interface MeterCollectDao {
     List<MeterCollect> selectPageByMap(Map<String,Object> paramsMap);
 
     List<Map<String,Object>> selectAllByMap(Map<String, Object> paramsMap);
+
+    List<MeterCollect>  checkCode(Map<String,Object> paramsMap);
+
+    List<MeterCollect>  checkSerialNo(String serialNo);
+
+    List<Map<String,Object>> getUnitInfo(String unitType);
 }
