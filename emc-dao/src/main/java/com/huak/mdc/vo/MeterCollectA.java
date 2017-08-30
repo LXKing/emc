9b78acbea8704a221003d16e146e171111c5415a
lichao <lichao@huak.com.cn>
2017-08-30 11:16:39 +0800

    vo

--- emc-dao/src/main/java/com/huak/mdc/vo/MeterCollectA.java
+++ emc-dao/src/main/java/com/huak/mdc/vo/MeterCollectA.java
@@ -7,14 +7,19 @@ import java.io.Serializable;
 public class MeterCollectA extends MeterCollect implements Serializable{
 
 
+    private static final long serialVersionUID = 9090351763739071516L;
+
+    public MeterCollectA(String id, String code, String name, String serialNo, String energyTypeId, Byte isreal, Byte istotal, String unitId, Byte unitType, Byte isauto, String depict, String tag, Byte coef, String formula, Byte isprestore, Byte isdelete, String comId, String unitname) {
+        super(id, code, name, serialNo, energyTypeId, isreal, istotal, unitId, unitType, isauto, depict, tag, coef, formula, isprestore, isdelete, comId);
+        this.unitname = unitname;
+    }
+
     private String unitname;
 
-    @Override
     public String getUnitname() {
         return unitname;
     }
 
-    @Override
     public void setUnitname(String unitname) {
         this.unitname = unitname;
     }
