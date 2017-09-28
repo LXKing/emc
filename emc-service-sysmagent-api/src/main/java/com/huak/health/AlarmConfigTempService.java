1917dab94970d157cedfd5b729e0203139d528b4
liurunming <67859086@qq.com>
2017-09-28 16:35:50 +0800

    室温配置

---
+++ emc-service-sysmagent-api/src/main/java/com/huak/health/AlarmConfigTempService.java
@@ -0,0 +1,41 @@
+package com.huak.health;
+
+import com.huak.common.page.Page;
+import com.huak.common.page.PageResult;
+import com.huak.health.model.AlarmConfig;
+import com.huak.health.model.AlarmConfigTemp;
+import com.huak.health.vo.AlarmConfigTempVo;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Copyright (C), 2009-2012, 北京华热科技发展有限公司.<BR>
+ * ProjectName:emc<BR>
+ * File name:  com.huak.health<BR>
+ * Author:  lichao  <BR>
+ * Project:emc    <BR>
+ * Version: v 1.0      <BR>
+ * Date: 2017/9/19<BR>
+ * Description: 报警配置    <BR>
+ * Function List:  <BR>
+ */
+public interface AlarmConfigTempService {
+    int deleteByPrimaryKey(String id);
+
+    int insert(AlarmConfig record);
+
+    int insertSelective(AlarmConfigTemp record);
+
+    AlarmConfig selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(AlarmConfigTemp record);
+
+    int updateByPrimaryKey(AlarmConfig record);
+
+    PageResult<AlarmConfigTempVo> queryByPage(Map<String, Object> paramsMap, Page page);
+
+    AlarmConfigTemp selectUpdateMap(String id);
+
+    List<Map<String,Object>> exportAlarmConfig(Map<String, Object> paramsMap);
+}
