493c652e7e987b98b15b79e636dc578d511dd943
lichao <lichao@huak.com.cn>
2017-10-11 15:02:27 +0800

    队列消息

---
+++ emc-dao/src/main/java/com/huak/health/model/PollingMessage.java
@@ -0,0 +1,35 @@
+package com.huak.health.model;
+
+import java.io.Serializable;
+
+/**
+ * Copyright (C), 2009-2012, 北京华热科技发展有限公司.<BR>
+ * ProjectName:emc<BR>
+ * File name:  com.huak.health.model<BR>
+ * Author:  lichao  <BR>
+ * Project:emc    <BR>
+ * Version: v 1.0      <BR>
+ * Date: 2017/10/11<BR>
+ * Description:   队列消息  <BR>
+ * Function List:  <BR>
+ */
+public class PollingMessage implements Serializable{
+    private String key;
+    private Object value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
