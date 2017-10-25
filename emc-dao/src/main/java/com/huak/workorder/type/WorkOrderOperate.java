802954cc3a4118b8a7d3c04b1e5134f958358ee1
Administrator <lichao@huak.com.cn>
2017-10-25 18:45:00 +0800

    工单翻译优化

--- emc-dao/src/main/java/com/huak/workorder/type/WorkOrderOperate.java
+++ emc-dao/src/main/java/com/huak/workorder/type/WorkOrderOperate.java
@@ -14,18 +14,18 @@ package com.huak.workorder.type;
 public enum WorkOrderOperate {
     A_SAVE(11,"保存工单"),
     A_SEND(12,"派送工单给"),
-    A_SAVE_SEND(13,"保存并派送工单"),
+    A_SAVE_SEND(13,"保存并派送工单给"),
     A_RESET(14,"重新派送工单，流程结束"),
     A_CONFIRM(15,"确认工单，流程结束"),
     A_CLOSE(16,"关闭工单，流程结束"),
 
     B_TAKING(21,"接受工单"),
-    B_BACK(22,"退回工单"),
-    B_SEND(23,"派送工单"),
+    B_BACK(22,"退回工单给"),
+    B_SEND(23,"派送工单给"),
     B_FINISH(24,"完成工单"),
 
     C_TAKING(31,"接受工单"),
-    C_BACK(32,"退回工单"),
+    C_BACK(32,"退回工单给"),
     C_FINISH(33,"完成工单");
 
     private int key;
