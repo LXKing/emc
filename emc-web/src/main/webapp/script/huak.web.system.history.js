6396ae669ed13392ca029a99e3ddaf1bfae345af
liuhe <1442576538@qq.com>
2017-11-23 15:03:14 +0800

    历史

--- emc-web/src/main/webapp/script/huak.web.system.history.js
+++ emc-web/src/main/webapp/script/huak.web.system.history.js
@@ -10,27 +10,50 @@ $(document).on("click", ".exportchange", function () {
 });
 function loadDataFun() {
     query(1);
+
+    $("body").off("click", ".x-sfbgbox1").on("click", ".x-sfbgbox1", function () {
+        $(this).next().stop(true, false).slideToggle(200, function () {
+        });
+    });
+    //下拉切换事件
+    $("body").off("click", ".x-sfoption1 p").on("click", ".x-sfoption1 p", function (event) {
+        var selectval = $(this).html();
+        var selectid = $(this).attr("value");
+
+        $(this).parent().siblings('input:hidden').val(selectid);
+        $(this).parent().prev().find("input").val(selectval);
+        $(this).parent().slideUp(200, function () {
+        });
+        event.stopPropagation();
+    });
+    $("body").on("mouseleave", ".x-selectfree", function (event) {
+        $(this).find(".x-sfoption1").slideUp(200, function () {
+        });
+        event.stopPropagation();
+    });
 }
 
+
+
+
 function reset(){
-   $("#unitName").val("");
-   $("#name").val("");
+    $("#power_type").siblings('.x-sfbgbox1').find(':input').val('请选择电厂类型');
+    $("#powertype").val("");
    $("#startTime").val("");
    $("#endTime").val("");
 }
+
+
 function query(num){
 
 //table 数据查询
-    var unitName=$("#unitName").val();
-    var name=$("#name").val();
     var sTime=$("#startTime").val();
     var eTime=$("#endTime").val();
-    console.log(unitName);
     $.ajax({
-        url: _web + '/record/change/list',
+        url: _web + '/history/data/list',
         type: 'post',
         async: true,//要指定不能异步,必须等待后台服务校验完成再执行后续代null码
-        data: {unitName:unitName,name:name,startTime:sTime,endTime:eTime,pageNumber:num},
+        data: {startTime:sTime,endTime:eTime,pageNumber:num},
         dataType: "json",
         success: function (data) {
             console.log(data.list);
